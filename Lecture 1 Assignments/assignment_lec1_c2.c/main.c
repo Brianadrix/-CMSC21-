85876dbe2a94f9d6d07b14ea25c4137a2207019e
Briana Jade Adricula <88810879+Brianadrix@users.noreply.github.com>
2022-03-08 19:24:43 +0800

    Add files via upload

---
+++ Lecture 1 Assignments/assignment_lec1_c2.c/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x, y, sub_result; // declares a variable //
+
+    // gives value to the variables //
+    x = 15;
+    y = 87;
+
+    // formula for subtraction //
+    sub_result = x-y;
+
+    // displays the result in the terminal //
+    printf("The subtraction of %d and %d is %d", x, y, sub_result);
+
+    return 0;
+}
